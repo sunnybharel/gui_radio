e5b628076c1371dcd1cd897fd92decc5b627cdde
sunny bharel <no@email.com>
2019-11-20 16:19:37 +0000

    Barebones gui experimentation and easygui added.

--- gui_radio.py
+++ gui_radio.py
@@ -1,4 +1,5 @@
 import subprocess
+from easygui import *
 
 
 def channel_choice():
@@ -10,7 +11,11 @@ def channel_choice():
 
 
 if __name__ == '__main__':
-    channel_choice()
+    msg = "Choose a Channel"
+    choices = ["Radio 1", "BBC 3", "BBC 4", "Classic FM"]
+    channel = buttonbox(msg, choices=choices)
+    msgbox(channel)
+
     process = subprocess.Popen(['mplayer', 'http://178.32.62.163:8469'],
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE,
